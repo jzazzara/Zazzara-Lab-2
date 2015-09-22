8ab757dcadc6ac63e778b4e584f47c1635a41388
unknown <4070324@HS222-12.vtsd.com>
2015-09-22 11:52:45 -0400

    commit

---
+++ Lab02Cv100.java
@@ -0,0 +1,20 @@
+public class Lab02Cv100
+{
+    public static void main (String args[])
+    {
+        System.out.println("Lab 02C, 100 Point Version");
+        System.out.println();
+        System.out.println("First Java Program");
+        System.out.println();
+        System.out.println("Measure today's knowledge ");
+        System.out.println("by yesterday's confusion.");
+        System.out.println();
+        System.out.println("Bewilderment + Exposure = Obvious");
+        System.out.println();
+        System.out.println("# ##### #####     ##### ##### # #    #  ##### #####");
+        System.out.println("# #   # #   #     #   # #   # # ##   #    #   #");
+        System.out.println("# #   # #   #     ##### #   # # # #  #    #   #####");
+        System.out.println("# #   # #   #     #     #   # # #  # #    #       #");
+        System.out.println("# ##### #####     #     ##### # #   ##    #   #####");
+    }
+}
---
+++ Lab02Cv80.java
@@ -0,0 +1,15 @@
+public class Lab02Cv80
+{
+    public static void main (String args[])
+    {
+        System.out.println("Lab 02C, 80 Point Version");
+        System.out.println();
+        System.out.println("First Java Program");
+        System.out.println();
+        System.out.println("Measure today's knowledge ");
+        System.out.println("by yesterday's confusion.");
+        System.out.println();
+        System.out.println("Bewilderment + Exposure = Obvious");
+        System.out.println();
+    }
+}
